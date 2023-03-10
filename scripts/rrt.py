f8381d023b06458fe1881a09721f52fcb55d8011
Alex <thatalexram@gmail.com>
2023-03-10 18:40:09 +0100

    RRT done, lacks backtrack

--- scripts/rrt.py
+++ scripts/rrt.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import rospy
-from random import random
-from numpy.linalg import norm
-from math import inf
+import random
+import math 
+from time import time
 
 from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Point
@@ -26,7 +26,6 @@ def isInObstacle(vex, radius): # This function name seems like a sketchy and sem
 path_through_obstacle_service = rospy.ServiceProxy('path_through_obstacle', isThroughObstacle)
 
 def isThruObstacle(p0, p1, radius):
-
     p0_pos = Point(p0[0], p0[1], 0.0)
     p1_pos = Point(p1[0], p1[1], 0.0)
 
@@ -57,20 +56,46 @@ def get_marker(type, pos, size, color, identity):
 
     return marker
 
-def nearest(vertex_list, goal_vertex):
-    prev_dist = inf
-    vex = None
+def nearest(vertex_list, new_vertex):
+    prev_dist = math.inf
+    nearest_vertex = None
 
-    for i, vertex in enumerate(vertex_list):
-        dist = norm((vertex[0], vertex[1]) - (goal_vertex[0], goal_vertex[1]))
+    for vertex in vertex_list:
+        dist = math.dist(vertex, new_vertex)
 
         if dist < prev_dist:
-            vex = vertex
+            nearest_vertex = vertex
+            prev_dist = dist
 
-        prev_dist = dist
 
-    return vex
-        
+    # print(f"Found nearest vertex {nearest_vertex}")
+    return nearest_vertex
+
+def nearest_obstacle(nearest_vertex, new_vertex, obstacle_radius):
+    '''
+    Shortes the a branch to fit within obstacles
+    '''
+    dist = math.dist(nearest_vertex, new_vertex)
+    angle = math.atan2(new_vertex[1] - nearest_vertex[1], new_vertex[0] - nearest_vertex[0])
+
+    while(isThruObstacle(nearest_vertex, new_vertex, obstacle_radius).throughObstacle or isInObstacle(new_vertex, obstacle_radius).inObstacle):
+          dist -= 0.1
+          new_vertex = (dist*math.cos(angle), dist*math.sin(angle))
+
+          if dist < 0:
+              break
+
+    return dist, new_vertex
+
+def backtrack(vertex_list, connected_list):
+    list_of_positions = []
+    list_of_positions.insert(0, vertex_list[-1])
+    connection = connected_list[-1]
+    while True:
+        prev_vertex = vertex_list[connection[0]]
+        list_of_positions.insert(0, prev_vertex)
+        connection = connected_list.index
+
 
 def get_edge_as_marker(first_point, second_point, color, identity, thickness=0.025):
 
@@ -83,13 +108,15 @@ def get_edge_as_marker(first_point, second_point, color, identity, thickness=0.0
     
     return edge_marker
 
-def RRT(startpos, endpos):
-    # RRT algorithm
+def RRT(startpos, endpos, step_length = 1, goal_radius=0.1):
+    '''
+    My implementation of the RRT algorithm
+    '''
 
     # Making markes on the map in real time
     tree_publisher = rospy.Publisher('tree_marker', MarkerArray, queue_size=10)
     tree_marker = MarkerArray()
-    marker_identity = 0
+    marker_identity = 0    
 
     # Create a blue-green square representing the start
     start_rgb_color = [0/256, 158/256, 115/256]
@@ -111,42 +138,75 @@ def RRT(startpos, endpos):
     vertex_list = []
     vertex_list.append(startpos) # The visualizer might not accept tuples
 
+    connected_list = []
+
+    num_iter = 0    
+    obstacle_radius = 0.3
+    dist = 0
+
     while (True):
-            random_vertex = Point(3*random(), 3*random(), 0.0)
-            obstacle_radius = 0.3
+            # Choosing random vertex to branch from
+            parent_vertex = random.choice(vertex_list)
+
+            new_vertex_direction = 2*math.pi*random.random()
+
+            new_vertex = (parent_vertex[0] + step_length*math.cos(new_vertex_direction), parent_vertex[1] + step_length*math.sin(new_vertex_direction))
+
+            num_iter += 1
 
             # End condition
-            if (norm((random_vertex[0], random_vertex[0]) - endpos) < obstacle_radius):
-                vertex_list.append(random_vertex)
+            if (math.dist(new_vertex, endpos) < goal_radius):
+                vertex_list.append(new_vertex)
                 break
-
+            
+            obs_response = isInObstacle(new_vertex, obstacle_radius).inObstacle
+            # print(f"Obstacle response: {obs_response}")
             # If vertex in obstacle go to next loop
-            if (isInObstacle(random_vertex, obstacle_radius)):
+            if (obs_response):
                continue
            
-            nearest_vertex = nearest(vertex_list, random_vertex)
+            nearest_vertex = nearest(vertex_list, new_vertex)
 
+            # if (isInObstacle(new_vertex, obstacle_radius).inObstacle or isThruObstacle(nearest_vertex, new_vertex, obstacle_radius).throughObstacle):
+            #     dist, new_vertex = nearest_obstacle(nearest_vertex, new_vertex, obstacle_radius)
+
+            # if dist < 0:
+            #     continue
+
+            thru_response = isThruObstacle(nearest_vertex, new_vertex, obstacle_radius).throughObstacle
+            # print(f"Thru response: {thru_response}")
             # Go to next loop if line crosses through obstacle
-            if (isThruObstacle(nearest_vertex, random_vertex)):
+            if (thru_response):
                continue
 
-            vertex_list.append(random_vertex)
+            vertex_list.append(new_vertex)
+            connected_list.append((vertex_list[nearest_vertex], len(vertex_list) - 1))
 
-            edge_marker = get_edge_as_marker(nearest_vertex, random_vertex, edge_color, marker_identity)
+            edge_marker = get_edge_as_marker(nearest_vertex, new_vertex, edge_color, marker_identity)
             marker_identity += 1
             tree_marker.markers.append(edge_marker)
+            if (num_iter % 10 == 0):
+                rospy.Rate(0.5).sleep() # needs to wait a bit for the publisher to start, a bit weird. 
+                tree_publisher.publish(tree_marker)
+                print(f"Iteration {num_iter} gave a valid vertex")
 
-
-    # Testing publishing markers outside the loop first, 
-    # would be cool to do inside the loop afterwards
+    
     rospy.Rate(0.5).sleep() # needs to a bit for the publisher to start, a bit weird. 
     tree_publisher.publish(tree_marker)
 
+    return num_iter, connected_list, vertex_list
+
+def traverse(pos_list):
+    position_control = rospy.ServiceProxy('/position_control', positionControl)
+
+    for position in pos_list:
+        rospy.wait_for_service('/position_control')
+        request = Point(position[0], position[1], 0.0)
+        response = position_control(request)
+        print(response)
 
 
 
-
-    
 
 if __name__ == '__main__':
     # -----------------
@@ -157,82 +217,92 @@ if __name__ == '__main__':
 
     # -----------------------------------------------
     # The start and end positions of the "short maze"
-    if (maze == "short_maze"):
-        startpos = (0.0, 0.0)
-        endpos = (4.5, 5.0)
-
-
+    startpos = (0.0, 0.0)
+    endpos = (4.5, 5.0)
 
     # -------------------------------------------------------------------------
     # The start and end positions of the bonus task with the "complicated maze"
-    if (maze == "complicated_maze"):
-        startpos = (0.0, 0.0)
-        endpos = (4.5, 9.0)
+    # startpos = (0.0, 0.0)
+    # endpos = (4.5, 9.0)
 
 
+    start_time = time()
+
+    num_iter, connected_list, vertex_list = RRT(startpos, endpos, 3, 0.05)
     
+    end_time = time()
+
+    print("---------- RRT Done ----------")
+    print(f"Finished in {num_iter} iterations")
+    print(f"Finished in {end_time - start_time} seconds")
+    
+    list_of_positions = backtrack(connected_list, vertex_list)
+
+    traverse(list_of_positions)
+
+
     # ---------------------------------------------------------------
-    # Example of how you can check if a point is inside an obstacle: 
+    # # Example of how you can check if a point is inside an obstacle: 
 
-    obstacle_radius = 0.3
-    vex_pos = [0.0, 0.0]
-    response = isInObstacle(vex_pos, obstacle_radius)
-    print("point_in_obstacle_service response: ")
-    print(response)
+    # obstacle_radius = 0.3
+    # vex_pos = [0.0, 0.0]
+    # response = isInObstacle(vex_pos, obstacle_radius)
+    # print("point_in_obstacle_service response: ")
+    # print(response)
 
 
     
-    # -----------------------------------------------------------------------
-    # Example of how you can check if the straightline path between two points 
-    # goes through an obstacle:
+    # # -----------------------------------------------------------------------
+    # # Example of how you can check if the straightline path between two points 
+    # # goes through an obstacle:
 
-    obstacle_radius = 0.3
-    first_point = [0.0, 0.0]
-    second_point = [1.0, 1.0]
-    response = isThruObstacle(first_point, second_point, obstacle_radius)
-    print("path_through_obstacle_service response: ")
-    print(response)
+    # obstacle_radius = 0.3
+    # first_point = [0.0, 0.0]
+    # second_point = [1.0, 1.0]
+    # response = isThruObstacle(first_point, second_point, obstacle_radius)
+    # print("path_through_obstacle_service response: ")
+    # print(response)
 
 
 
-    # -----------------------------------------------------------------------
-    # Example of how you can visualize a graph using a MarkerArray publisher:
+    # # -----------------------------------------------------------------------
+    # # Example of how you can visualize a graph using a MarkerArray publisher:
 
-    list_of_positions = [[0.0, 0.0], [0.0, 2.0], [1.0, 2.0], [1.0, 1.0]]
+    # list_of_positions = [[0.0, 0.0], [0.0, 2.0], [1.0, 2.0], [1.0, 1.0]]
 
-    tree_publisher = rospy.Publisher('tree_marker', MarkerArray, queue_size=10)
-    tree_marker = MarkerArray()
-    marker_identity = 0
+    # tree_publisher = rospy.Publisher('tree_marker', MarkerArray, queue_size=10)
+    # tree_marker = MarkerArray()
+    # marker_identity = 0
 
-    # Create a blue-green square representing the start
-    start_rgb_color = [0/256, 158/256, 115/256]
-    start_marker_size = 0.2
-    start_marker = get_marker(Marker.CUBE, list_of_positions[0], start_marker_size, start_rgb_color, marker_identity)
-    marker_identity += 1
+    # # Create a blue-green square representing the start
+    # start_rgb_color = [0/256, 158/256, 115/256]
+    # start_marker_size = 0.2
+    # start_marker = get_marker(Marker.CUBE, list_of_positions[0], start_marker_size, start_rgb_color, marker_identity)
+    # marker_identity += 1
 
-    # Create a vermillion square representing the goal
-    end_rgb_color = [213/256, 94/256, 0/256]
-    end_marker_size = 0.2
-    end_marker = get_marker(Marker.CUBE, list_of_positions[-1], end_marker_size, end_rgb_color, marker_identity)
-    marker_identity +=1
+    # # Create a vermillion square representing the goal
+    # end_rgb_color = [213/256, 94/256, 0/256]
+    # end_marker_size = 0.2
+    # end_marker = get_marker(Marker.CUBE, list_of_positions[-1], end_marker_size, end_rgb_color, marker_identity)
+    # marker_identity +=1
 
-    tree_marker.markers.append(start_marker)
-    tree_marker.markers.append(end_marker)
+    # tree_marker.markers.append(start_marker)
+    # tree_marker.markers.append(end_marker)
 
     
-    # Create reddish purple edges 
-    edge_color = [204/256, 121/256, 167/256]
+    # # Create reddish purple edges 
+    # edge_color = [204/256, 121/256, 167/256]
     
-    for index in range(len(list_of_positions)-1):
-        first_point = list_of_positions[index]
-        second_point = list_of_positions[index+1]
-        edge_marker = get_edge_as_marker(first_point, second_point, edge_color, marker_identity)
-        marker_identity += 1
-        tree_marker.markers.append(edge_marker)
+    # for index in range(len(list_of_positions)-1):
+    #     first_point = list_of_positions[index]
+    #     second_point = list_of_positions[index+1]
+    #     edge_marker = get_edge_as_marker(first_point, second_point, edge_color, marker_identity)
+    #     marker_identity += 1
+    #     tree_marker.markers.append(edge_marker)
 
 
-    rospy.Rate(0.5).sleep() # needs to a bit for the publisher to start, a bit weird. 
-    tree_publisher.publish(tree_marker)
+    # rospy.Rate(0.5).sleep() # needs to a bit for the publisher to start, a bit weird. 
+    # tree_publisher.publish(tree_marker)
 
 
 
@@ -245,11 +315,11 @@ if __name__ == '__main__':
     # It is also not very well tuned (and quite slow).
 
 
-    list_of_position = [[0.0, 2.0], [1.0, 2.0], [1.0, 1.0]]
-    position_control = rospy.ServiceProxy('/position_control', positionControl)
+    # list_of_position = [[0.0, 2.0], [1.0, 2.0], [1.0, 1.0]]
+    # position_control = rospy.ServiceProxy('/position_control', positionControl)
 
-    for position in list_of_position:
-        rospy.wait_for_service('/position_control')
-        request = Point(position[0], position[1], 0.0)
-        response = position_control(request)
-        print(response)
+    # for position in list_of_position:
+    #     rospy.wait_for_service('/position_control')
+    #     request = Point(position[0], position[1], 0.0)
+    #     response = position_control(request)
+    #     print(response)
